c5974098dc1b064fadb90185ca0fa133348bd550
greeknerd1 <gabykh@berkeley.edu>
2024-09-02 15:53:52 -0700

    Fixing bug in StockDetails to get correct ticker symbol from URL

--- frontend/src/components/StockDetails.js
+++ frontend/src/components/StockDetails.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 function StockDetails() {
-  const { ticker_symbol } = useParams();
+  const { tickerSymbol } = useParams();
 
   // Fetch and display stock details using ticker_symbol
   // This could include charts, historical data, etc.
 
   return (
     <div>
-      <h2>Details for {ticker_symbol}</h2>
+      <h2>Details for {tickerSymbol}</h2>
       {/* Display stock details here */}
     </div>
   );
