75d58b4d165fe0ee88607be082e4341d03ff8143
greeknerd1 <gabykh@berkeley.edu>
2024-09-22 10:56:27 -0700

    Ensuring that db is updated with latest stock data whenever we launch the backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pool = require('./db');
 const cors = require('cors');
+require('./updateStocks.js')
 
 const app = express();
 const port = process.env.PORT || 5000;
